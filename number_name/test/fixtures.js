6c8cad4194eb18a7d52c5e15829128cde0f679cf
emagrorrom <373407641@qq.com>
2016-03-01 16:25:52 +0800

    Create fixtures.js

---
+++ number_name/test/fixtures.js
@@ -0,0 +1,14 @@
+function loadSpecialNumberStr() {
+  return [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+    'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
+}
+
+function loadTens() {
+  return [
+    '', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy',
+    'eighty', 'ninety'
+  ];
+}
