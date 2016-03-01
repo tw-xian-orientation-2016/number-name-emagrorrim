13872cb1301ae7dd0e0a69e257404bb46c39a6e0
emagrorrom <373407641@qq.com>
2016-03-01 16:27:51 +0800

    finish splitNumberString Test

--- number_name/test/task-test.js
+++ number_name/test/task-test.js
@@ -2,18 +2,18 @@ describe('splitNumberString', function() {
   var inputs;
 
   beforeEach(function() {
-    inputs = ['43112603', '1001', '2'];
+    inputs = ['3112603', '1001', '2'];
   });
 
   it('should split number into array', function() {
 
     var outputs = [];
     for (var i = 0; i < inputs.length; i++) {
-      outputs.push(sp)
+      outputs.push(splitNumberString(inputs[i]));
     }
 
     var expectObject = [
-      ['43', '112', '603'],
+      ['3', '112', '603'],
       ['1','001'],
       ['2']
     ];
