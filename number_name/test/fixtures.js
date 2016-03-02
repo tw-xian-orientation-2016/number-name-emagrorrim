3f56617bd441fc545981b52ef0c291f85f44058c
emagrorrom <373407641@qq.com>
2016-03-02 08:50:06 +0800

    finish translateNumberToName Test

--- number_name/test/fixtures.js
+++ number_name/test/fixtures.js
@@ -2,7 +2,7 @@ var partLength = 3;
 
 function loadDigitsWords() {
   return [
-    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
     'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
     'sixteen', 'seventeen', 'eighteen', 'nineteen'
   ];
--- number_name/test/task-test.js
+++ number_name/test/task-test.js
@@ -26,8 +26,9 @@ describe('translateNumbers', function() {
   var inputs;
 
   beforeEach(function() {
-    inputs = ['', '43', '123'];
+    inputs = ['12', '43', '123', '000'];
     digitsWords = loadDigitsWords();
+    tensWords = loadTensWords();
   });
 
   it('shoult translate hundred', function() {
@@ -40,9 +41,46 @@ describe('translateNumbers', function() {
     var expectObject = [
       '',
       '',
-      'one hundred'
+      'one hundred',
+      ''
     ];
 
     expect(outputs).toEqual(expectObject);
   });
+
+  it('shoult translate tens and digits', function() {
+
+    var outputs = [];
+    for (var i = 0; i < inputs.length; i++) {
+      outputs.push(translateTensAndDigits(inputs[i], digitsWords, tensWords));
+    }
+
+    var expectObject = [
+      'twelve',
+      'fourty three',
+      'twenty three',
+      ''
+    ];
+
+    expect(outputs).toEqual(expectObject);
+  });
+
+  it('shoult translate numbers', function() {
+    inputs = [
+      ['12', '000','001'],
+      ['2', '014', '245']
+    ];
+    var outputs = [];
+    for (var i = 0; i < inputs.length; i++) {
+      outputs.push(translateNumbers(inputs[i], digitsWords, tensWords));
+    }
+
+    var expectObject = [
+      ['twelve', '', 'one'],
+      ['two', 'fourteen', 'two hundred and fourty five']
+    ];
+
+    expect(outputs).toEqual(expectObject);
+  });
+
 });
